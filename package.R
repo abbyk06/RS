c9b0ad6409289a63060bbdd06ebffcb540f7294b
Abigail <abbyskim.work@gmail.com>
2025-11-13 17:03:50 -0500

    fixing package, adding terminal

--- package.R
+++ package.R
@@ -1,11 +1,15 @@
 packages <- c("shiny", "shinythemes", "ggplot2", "remotes")
-for (pkg in packages) {
-    if (!require(pkg, character.only = TRUE)) {
-    install.packages(pkg, dependencies = TRUE, repos = "https://cloud.r-project.org")
+    for (pkg in packages) {
+        if (!require(pkg, character.only = TRUE)) {
+        install.packages(pkg, dependencies = TRUE, repos = "https://cloud.r-project.org")
     }
 }
 
 library(shiny)
 library(shinythemes)
 library(ggplot2)
-remotes::install_github("rstudio/shinytableau")
+library(remotes)
+
+if (!require("shinytableau", character.only = TRUE)) {
+    remotes::install_github("rstudio/shinytableau")
+}
